2dcf8fe694dc83c802a41b481acc66254ae3cdbb
S5 <carlos.diaz@sinco.com.co>
2018-03-06 16:43:34 -0500

    Avance diseño
    Avance diseño

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,28 @@
 const market = s5.initialize();
 
 market.require([], () => {
-    s5.get('.login').shift().insert(s5.iconos.Tabular(20, '#FFFFFF'), 0);
-    s5.get('.signin').shift().insert(s5.iconos.Perfil(20, '#FFFFFF'), 0);
+    const authcontainer = s5.get('.authentication-container').shift();
+    const logincontainer = s5.get('.login-container').shift();
+    const login = s5.get('login').insert(s5.iconos.Tabular(20, '#FFFFFF'), 0);
+    const signin = s5.get('signin').insert(s5.iconos.Perfil(20, '#FFFFFF'), 0);
+    const backs = s5.get('.back, .in');
+
+    signin.__container = '.signin-container';
+    login.__container = '.login-container';
+
+    const __click = function () {
+        s5.get('.container:not({0})'.format(this.__container)).forEach((c) => c.classList.add('hide'));
+
+        s5.get(this.__container).shift().classList.remove('hide');
+    };
+
+    login.addEvent('click', __click);
+    signin.addEvent('click', __click);
+
+    backs.forEach((btn) =>{
+        btn.insert(s5.iconos.Flecha(12, '#FFFFFF'), 0);
+        btn.__container = '.authentication-container';
+        if (btn.classList.contains('back'))
+            btn.addEvent('click', __click);
+    });
 });
