9c8b14c13f6da35079426185dbd3367e89178c9d
S5 <carlos.diaz@sinco.com.co>
2018-03-07 15:52:19 -0500

    a
    a

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,30 +3,13 @@
 /// <reference path="s5-js/s5.autocomplete.js" />
 
 const market = s5.initialize();
+market['services-url'] = 'https://eli-market-s.herokuapp.com';
 
-market.require([], () => {
-    const authcontainer = s5.get('.authentication-container').shift();
-    const logincontainer = s5.get('.login-container').shift();
-    const login = s5.get('login').insert(s5.iconos.Tabular(20, '#FFFFFF'), 0);
-    const signin = s5.get('signin').insert(s5.iconos.Perfil(20, '#FFFFFF'), 0);
-    const backs = s5.get('.back, .in');
+const login = s5.get('login').insert(s5.iconos.Tabular(20, '#FFFFFF'), 0);
+const signin = s5.get('signin').insert(s5.iconos.Perfil(20, '#FFFFFF'), 0);
+const backs = s5.get('.back, .in');
+backs.forEach((btn) => btn.insert(s5.iconos.Flecha(12, '#FFFFFF'), 0));
 
-    signin.__container = '.signin-container';
-    login.__container = '.login-container';
-
-    const __click = function () {
-        s5.get('.container:not({0})'.format(this.__container)).forEach((c) => c.classList.add('hide'));
-
-        s5.get(this.__container).shift().classList.remove('hide');
-    };
-
-    login.addEvent('click', __click);
-    signin.addEvent('click', __click);
-
-    backs.forEach((btn) =>{
-        btn.insert(s5.iconos.Flecha(12, '#FFFFFF'), 0);
-        btn.__container = '.authentication-container';
-        if (btn.classList.contains('back'))
-            btn.addEvent('click', __click);
-    });
+market.require(['index/layout'], (layout) => {
+    layout.init();
 });
