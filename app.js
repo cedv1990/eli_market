f2fec1374a033c55e82df957d947cdccc678392a
S5 <carlos.diaz@sinco.com.co>
2018-03-08 15:45:50 -0500

    A
    A

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "*");
   req.header("Access-Control-Allow-Origin", "*");
   req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  req.header("Access-Control-Allow-Methods", "*");
   next();
 });
 
