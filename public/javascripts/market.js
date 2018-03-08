ef91f118255b873e04c30ad69158333ef3de101f
S5 <carlos.diaz@sinco.com.co>
2018-03-08 15:41:35 -0500

    S
    S

--- public/javascripts/market.js
+++ public/javascripts/market.js
@@ -9,6 +9,8 @@ market.require([], () => {
     const btnUser = s5.get('.user-button').shift();
     const menuUser = s5.get('.user-menu').shift();
     const logout = s5.get('.exit').shift().insert(s5.iconos.Power(12, '#FFFFFF'), 0);
+    const photo = s5.get('.photo').shift();
+    const fileInput = s5.get('photo-uploader');
 
     btnUser.addEvent('click', () => {
         btnUser.classList.toggle('selected');
@@ -17,5 +19,26 @@ market.require([], () => {
 
     logout.addEvent('click', () => {
         window.location.href = '/abandon';
+    });
+
+    photo.addEvent('click', () => fileInput.click());
+
+    fileInput.addEvent('change', () => {
+        if (fileInput.files.length > 0){
+            s5.fileToBase64(fileInput.files[0], (f) => {
+                const data = 'data:image/{0};base64,{1}'.format(f.name.split('.').pop().toLowerCase(), f.src);
+                s5.Request('PATCH', market['services-url'] + '/user', {
+                    Ok: (d) => {
+                        debugger;
+                    }
+                },
+                {
+                    'user': {
+                        'photo': data,
+                        'cod': parseInt( fileInput.attribute('data-cod') )
+                    }
+                })
+            });
+        }
     });
 });
